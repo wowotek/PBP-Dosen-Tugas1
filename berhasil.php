809471db5b282c02e9413fc0ab19977db6075941
Erlangga Ibrahim <erlanggaibr2@gmail.com>
2019-09-11 02:15:16 +0700

    Started doing backend, second page

---
+++ berhasil.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PBP Tugas-1</title>
+    <script src="res/fontawesome-all.js"></script>
+    <link rel="stylesheet" href="res/bulma.min.css">
+</head>
+<body>
+<?php
+
+$KODE_PRODI = array(
+    "67" => "Teknologi Informatika",
+    "68" => "Sistem Informasi"
+);
+
+echo $KODE_PRODI["67"];
+
+?>
+</body>
