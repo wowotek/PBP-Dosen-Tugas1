86bc35b92a1b28e3c5395b02485273c00dedad2f
Erlangga Ibrahim <erlanggaibr2@gmail.com>
2019-09-13 16:10:10 +0700

    removed *.js file for security reasons

--- berhasil.php
+++ berhasil.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>PBP Tugas-1</title>
-    <script src="res/fontawesome-all.js"></script>
     <link rel="stylesheet" href="res/bulma.min.css">
 </head>
 
