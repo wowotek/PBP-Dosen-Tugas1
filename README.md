0212615815cc2fd64ebcf715b5203a423576edca
Erlangga Ibrahim <erlanggaibr2@gmail.com>
2019-09-13 15:50:17 +0700

    finished

--- README.md
+++ README.md
@@ -1,24 +1,24 @@
 # PBP-Dosen-Tugas1
 
-> Tugas Ini di rilis oleh  **Erlangga Ibrahim** secara terbuka (*Open Source*)
-> Dibawah Lisensi **GNU GPLv3**. untuk keterangan dan informasi lebih lanjut mengenai
+> Tugas Ini di rilis oleh  Erlangga Ibrahim secara terbuka (*Open Source*)
+> Dibawah Lisensi GNU GPLv3. untuk keterangan dan informasi lebih lanjut mengenai
 > Lisensi, dapat dibuka di : https://www.gnu.org/licenses/gpl-3.0.en.html
 > atau dengan membaca file `LICENSE`
 >  
 > Semua sumber yang digunakan pada tugas ini, dapat dilihat pada :
 > https://github.com/wowotek/PBP-Dosen-Tugas1
 >  
-> **PERINGATAN** : *Tugas ini dirilis **setelah tenggat waktu** pengumpulan tugas, dan tidak akan berlaku untuk kolega dengan tenggat waktu pengumpulan yang sama*
+> PERINGATAN : *Tugas ini dirilis setelah tenggat waktu pengumpulan tugas, dan tidak akan berlaku untuk kolega dengan tenggat waktu pengumpulan yang sama*
 >  
-> **PERINGATAN** : *Tugas ini dirilis **tanpa garansi**, termasuk nilai yang di-nihilkan oleh sebab **mencontek** dan/atau **menyalin** dan/atau **meniru** yang bersumber dari tugas ini*
+> PERINGATAN : *Tugas ini dirilis tanpa garansi, termasuk nilai yang di-nihilkan oleh sebab mencontek dan/atau menyalin dan/atau meniru yang bersumber dari tugas ini*
 
 ## Pengawas / Dosen
 
-* **Yerymia Alfa Susetyo** - *-*, *yere13@gmail.com*
+* Yerymia Alfa Susetyo - *-*, *yere13@gmail.com*
 
 ## Penulis
 
-* **Erlangga Ibrahim** (672017282) - *@wowotek*
+* Erlangga Ibrahim (672017282) - *@wowotek*
 
 ## Deskripsi Tugas
 
@@ -31,6 +31,6 @@
   * _Text_/_Number_ Input `NIM`
   * 2 Buah Radio Button `Jenis Kelamin`
   * *Submit* Input (Tombol) `Submit`
-* Laman Hasil Terdiri dari hasil data yang di atur menjadi sebuah _**Greetings**_
+* Laman Hasil Terdiri dari hasil data yang di atur menjadi sebuah _Greetings_
 ### Lampiran Foto Penjelasan Tugas
 ![tugas](tugas.jpeg)
--- berhasil.php
+++ berhasil.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" style="overflow:hidden">
 
 <head>
     <meta charset="UTF-8">
@@ -9,15 +9,146 @@
     <script src="res/fontawesome-all.js"></script>
     <link rel="stylesheet" href="res/bulma.min.css">
 </head>
-<body>
-<?php
 
-$KODE_PRODI = array(
-    "67" => "Teknologi Informatika",
-    "68" => "Sistem Informasi"
-);
+<body class="has-background-success" style="height:100vh;">
+    <?php
+    require("constant.php");
 
-echo $KODE_PRODI["67"];
+    // Requests Data
+    $name = ucfirst($_POST["name_input"]);
+    $nim = $_POST["nim_input"];
+    $gender = $_POST["gender_radio"];
 
-?>
+    // Data Process
+    $nama_depan = explode(" ", $name)[0];
+    $digit_prodi = $nim[0] . $nim[1];
+    $digit_angkatan = $nim[2] . $nim[3] . $nim[4] . $nim[5];
+    $prodi = "Program Studi Yang Anda Pilih Tidak Tersedia";
+    $jk = $gender == "pria" ? "Mahasiswa" : "Mahasiswi";
+
+    if (isset($KODE_PRODI[$digit_prodi])) {
+        $prodi = $KODE_PRODI[$digit_prodi];
+    }
+
+    ?>
+
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+            <a class="navbar-item" href="https://bulma.io">
+                <img src="./res/header_logo.png" width="112" height="28">
+            </a>
+
+            <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+            </a>
+        </div>
+
+        <div id="navbarBasicExample" class="navbar-menu">
+            <div class="navbar-start">
+                <a class="navbar-item" href="./">
+                    Daftar Ulang
+                </a>
+            </div>
+
+            <div class="navbar-end">
+                <div class="navbar-item">
+                    <div class="buttons">
+                        <button class="button is-primary" disabled>
+                            <strong>Pendaftaran Mahasiswa</strong>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <div class="container has-background-danger" style="height: 100%">
+        <br>
+        <h1 class="title has-text-centered has-text-white">
+            Selamat Datang <?php echo $nama_depan ?> !
+        </h1>
+        <hr>
+        <div class="columns">
+            <div class="column">
+                <div class="box" style="margin: 10px; height: 100%">
+                    <h1 class="title has-text-centered has-text-info">
+                        Informasi
+                    </h1>
+                    <div class="box">
+                        <article class="media">
+                            <figure class="media-left">
+                                <p class="image is-64x64">
+                                    <img src="./res/uksw.png">
+                                </p>
+                            </figure>
+                            <div class="media-content">
+                                <div class="content">
+                                    <p>
+                                        <strong>Administrasi UKSW</strong> <small>@infoBaraUKSW</small> <small>31m</small>
+                                        <br>
+                                        Anda telah berhasil terdaftar sebagai <?php echo $jk . " " . $prodi ?>
+                                    </p>
+                                </div>
+                            </div>
+                        </article>
+                    </div>
+                    <div class="box">
+                        <article class="media">
+                            <figure class="media-left">
+                                <p class="image is-64x64">
+                                    <img src="./res/fti.jpg">
+                                </p>
+                            </figure>
+                            <div class="media-content">
+                                <div class="content">
+                                    <p>
+                                        <strong>HMPTI</strong> <small>@hmp_ti</small> <small>31m</small>
+                                        <br>
+                                        Waaah... Anak TI Baru ya ? Jangan Lupa Daftar FTI-Days Ya Dek <?php echo $name ?>!
+                                    </p>
+                                </div>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+            </div>
+            <div class="column">
+                <div class="box" style="margin: 10px; height: 100%">
+                    <h1 class="title has-text-centered has-text-info">
+                        Biodata Diri
+                    </h1>
+                    <div class="field">
+                        <label class="label">Nama Lengkap</label>
+                        <div class="control">
+                            <input class="input" type="text" value="<?php echo $name ?>" disabled>
+                        </div>
+                    </div>
+                    <div class="field">
+                        <label class="label">Nomer Induk Mahasiswa</label>
+                        <div class="control">
+                            <input class="input" type="text" value="<?php echo $nim ?>" disabled>
+                        </div>
+                    </div>
+                    <div class="field">
+                        <label class="label">Program Studi</label>
+                        <div class="control">
+                            <input class="input" type="text" value="<?php echo $prodi ?>" disabled>
+                        </div>
+                    </div>
+                    <div class="field">
+                        <label class="label">Jenis Kelamin</label>
+                        <div class="control">
+                            <div class="select">
+                                <select name="gend" disabled>
+                                    <option value=""><?php echo ucfirst($gender) ?></option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
 </body>
